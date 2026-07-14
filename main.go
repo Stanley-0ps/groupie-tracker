43d93ef2af03c0941ee96b0e204022edd23ca5f4
Stanley-0ps <ochaistanley7@gmail.com>
2026-07-14 10:46:44 +0100

    Initialized a web server

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+
+	log.Println("Server running on http://localhost:8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Groupie Tracker"))
+}
